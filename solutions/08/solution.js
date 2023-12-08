efff6efa37ad05b012d7897bac3349c37e04a68d
Homer Painter <homer.painter@simplethread.com>
2023-12-08 16:55:53 -0500

    day 8

--- solutions/08/solution.js
+++ solutions/08/solution.js
@@ -1,24 +1,85 @@
 /**
- * Advent of Code 2023 Day X
- * https://adventofcode.com/2023/day/X
+ * Advent of Code 2023 Day 8
+ * https://adventofcode.com/2023/day/8
  */
 const { readFile } = require('../../utils.js');
 
 /**
- * ...
+ * You put your left foot in, you take your left foot out.
  */
 const solve = async (partNumber=1, inputFile='./input.txt') => {
-  const input = await readFile(inputFile);
+  const [turns, nodes] = parseInput(await readFile(inputFile));
+
   let solution;
   switch (+partNumber) {
     case 1:
-      // solve part 1
+      // Part 1 requests steps needed to travel from node AAA to ZZZ
+      solution = stepsBetween('AAA', /ZZZ/, 0, nodes, turns);
       break;
+
     case 2:
-      // solve part 2
+      // Part 2 requests steps needed to simultaneously travel from every ..A
+      // node until all paths are at ..Z nodes
+      let nodesA = [...nodes.keys()].filter(p => p.endsWith('A'));
+      let nodesZ = [...nodes.keys()].filter(p => p.endsWith('Z'));
+
+      // step count for all ..A nodes to first ..Z:
+      let steps = nodesA.map(n => stepsBetween(n, /Z$/, 0, nodes, turns));
+
+      // find lowest common multiple of all the step counts:
+      solution = steps.reduce(least_common_multiple)
       break;
   }
   return solution;
+};
+
+/**
+ * Not gonna lie, I stole these two nifty one liners from stack overflow
+ */
+const greatest_common_denom = (a, b) => a ? greatest_common_denom(b % a, a) : b;
+const least_common_multiple = (a, b) => a * b / greatest_common_denom(a, b);
+
+/**
+ * Get the number of steps needed to travel from a starting node to a node that
+ * matches an ending pattern.
+ * 
+ * @param {string} start - starting node
+ * @param {RegExp} end - ending node match pattern
+ * @param {integer} startTurn - turns index
+ * @param {array} nodes - full node list reference
+ * @param {array} turns - full turns list reference
+ * @return {integer}
+ */
+const stepsBetween = (start, end, startTurn, nodes, turns) => {
+  let [numSteps, turnIdx, position] = [0, startTurn, start];
+  while (true) {
+    const lastPosition = position;
+    position = nodes.get(position)[turns[turnIdx]];
+    if(!position) { console.log(lastPosition, startTurn, turnIdx, turns[turnIdx]); }
+    turnIdx += 1;
+    numSteps += 1;
+    if (turnIdx == turns.length) { turnIdx = 0; }
+    if(position.match(end) !== null) { break; }
+  }
+  return numSteps;
+};
+
+/**
+ * Parse input file into list of turns and nodes.
+ *
+ * turns format: ['L', 'R', ...]
+ * nodes format: <Map>{'AAA': ['BBB', 'CCC'], 'BBB': ['DDD', 'EEE'], ...}
+ */
+const parseInput = (input) => {
+  const turns = input[0].split('').map(t => t === 'L' ? 0 : 1);
+  const nodes = new Map();
+  input.slice(2).forEach((line) => {
+    const nodeMatch = line.match(
+      /([A-Z]{3})\s+=\s+\(([A-Z]{3}),\s+([A-Z]{3})\)/
+    );
+    nodes.set(nodeMatch[1],[nodeMatch[2], nodeMatch[3]]);
+  });
+  return [turns, nodes];
 };
 
 module.exports = { solve }
